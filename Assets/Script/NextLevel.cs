cb642e254190bf67a2a8d5bf6498ea9b50ad74e7
LucaSchifano <luca.schifano005@gmail.com>
2021-09-08 15:20:15 +0200

    checkpoint + camera + level

---
+++ Assets/Script/NextLevel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+
+    public string levelName;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
